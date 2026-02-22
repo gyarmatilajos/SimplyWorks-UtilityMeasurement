5066eb36118f023cdc02b3453d8c665ca56ad1c0
gyarmatilajos <lajos@gyarmati.co>
2026-02-22 14:27:23 +0100

    Add project files.

---
+++ SimplyWorks-UtilityMeasurement.Maui/Platforms/Android/MainApplication.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace SimplyWorks_UtilityMeasurement.Maui
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ SimplyWorks-UtilityMeasurement.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SimplyWorks_UtilityMeasurement.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
